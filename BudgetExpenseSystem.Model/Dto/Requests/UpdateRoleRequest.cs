3f757df4f4a32b2f8edccee0a8948d80ceb1381b
SofijaVvv <sofija.vujosevic@gmail.com>
2024-11-30 13:10:33 +0100

    Removed two properties from UpdateRoleRequest

--- BudgetExpenseSystem.Model/Dto/Requests/UpdateRoleRequest.cs
+++ BudgetExpenseSystem.Model/Dto/Requests/UpdateRoleRequest.cs
@@ -2,9 +2,5 @@ namespace BudgetExpenseSystem.Model.Dto.Requests;
 
 public class UpdateRoleRequest
 {
-    public int Id { get; set; }
-    
     public string Name { get; set; }
-    
-    public DateOnly CreatedDate { get; set; } 
 }
