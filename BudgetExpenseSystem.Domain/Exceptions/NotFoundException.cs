8333c29a6a976203f9cea4a840c0b8e0bba21f9c
SofijaVvv <sofija.vujosevic@gmail.com>
2024-11-30 13:01:16 +0100

    Added NotFoundException

--- BudgetExpenseSystem.Domain/Exceptions/NotFoundException.cs
+++ BudgetExpenseSystem.Domain/Exceptions/NotFoundException.cs
@@ -4,5 +4,4 @@ public class NotFoundException : Exception
 {
     public NotFoundException(string message) : base(message) { }
     
-    public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
 }
